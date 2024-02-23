02e71b772baa615629f36984e758b70ce71ccac9
Koichi Kaneshige <coarse.ground@gmail.com>
2024-02-23 16:39:06 +0900

    Enable to change the port number

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
+	// No need to set `Addr` field, because we use the listener passed as an argument
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -21,7 +23,7 @@ func run(ctx context.Context) error {
 	// Start the server in a goroutine
 	eg.Go(func() error {
 		// http.ErrServerClosed means the server was closed gracefully, so it's not an error.
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
@@ -40,7 +42,18 @@ func run(ctx context.Context) error {
 }
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("Specify the port number as the first argument")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+
+	if err := run(context.Background(), l); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
 	}
 }
