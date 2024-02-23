40e23c0f7721ea089da296041ce5ab891d3bf35b
Koichi Kaneshige <coarse.ground@gmail.com>
2024-02-23 18:27:05 +0900

    Create Server struct

--- server_test.go
+++ server_test.go
@@ -12,8 +12,6 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	t.Skip("Refactoring...")
-
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("failed to listen: %v", err)
@@ -21,8 +19,13 @@ func TestRun(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
 	eg.Go(func() error {
-		return run(ctx)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 
 	in := "message"
