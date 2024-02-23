51c1bceba0bdd5d6a598a568fdf35e9b4de532e5
Koichi Kaneshige <coarse.ground@gmail.com>
2024-02-23 16:26:59 +0900

    Create run function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tetran/go-web-app-example
 
 go 1.21.1
+
+require golang.org/x/sync v0.6.0
--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v\n", err)
-		os.Exit(1)
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	// Start the server in a goroutine
+	eg.Go(func() error {
+		// http.ErrServerClosed means the server was closed gracefully, so it's not an error.
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+
+		return nil
+	})
+
+	// Wait for the context to be done
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// Wait for the goroutine to finish
+	return eg.Wait()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
 	}
 }
