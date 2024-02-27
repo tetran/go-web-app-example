4480ee84d3fa2bdeed0280e195231655d0e5d141
Koichi Kaneshige <coarse.ground@gmail.com>
2024-02-27 22:32:11 +0900

    Make middleware

---
+++ handler/middleware.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/tetran/go-web-app-example/auth"
+)
+
+func AuthMiddleware(j *auth.JWTer) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			req, err := j.FillContext(r)
+			if err != nil {
+				RespondJSON(r.Context(), w, ErrResponse{
+					Message: "could not find auth info",
+					Details: []string{err.Error()},
+				}, http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func AdminMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !auth.IsAdmin(r.Context()) {
+			RespondJSON(r.Context(), w, ErrResponse{
+				Message: "admin required",
+			}, http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
